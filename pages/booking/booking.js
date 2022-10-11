6203b9fc488d40c3adf13fb8d7c2d94c0ac9b9c4
Shortytheman <shor7ty@gmail.com>
2022-10-11 11:07:00 +0200

    gg

---
+++ pages/booking/booking.js
@@ -0,0 +1,18 @@
+document.getElementsByTagName("tr").onclick = colorCheck
+
+
+function colorCheck(evt) {
+    const targetId = target.getElementById
+    console.log(targetId)
+
+}
+const target = evt.target
+
+const btn = document.getElementById(target.getElementById);
+
+btn.addEventListener('click', function onClick() {
+  btn.style.backgroundColor = 'green';
+});
+
+
+
