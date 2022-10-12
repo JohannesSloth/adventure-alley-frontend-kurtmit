1d34f246697a84e9da0e5fa200ffa544c274cdfe
Hyttel <mikkel.hw@hotmail.com>
2022-10-12 11:30:30 +0200

    Mikkel - småændringer som ikke virker

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,11 +1,17 @@
+import {employeeURL} from "../../util.js";
+
+
 const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-loginButton.addEventListener("click", (e) => {
+loginButton.addEventListener("click", async (e) => {
     e.preventDefault();
     const username = loginForm.username.value;
     const password = loginForm.password.value;
+
+    const employeeLogin = await fetch(employeeURL).then(e => e.json())
+
 
     if (username === "user" && password === "web_dev") {
         alert("You have successfully logged in.");
